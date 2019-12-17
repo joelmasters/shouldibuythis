a3a128a3e444f3cc7b946b0a24b4b02e02c025e1
joelmasters <joelmasters@gmail.com>
2019-12-16 19:01:26 -0800

    Updates

---
+++ precache-manifest.35bed320dcf411c12cee0120b8d95d8c.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd8da48368eab753ea9aade91b1c9e6",
+    "url": "/shouldibuythis/index.html"
+  },
+  {
+    "revision": "dc272ad5a470eb7b89f8",
+    "url": "/shouldibuythis/static/js/main.f2ed4997.chunk.js"
+  },
+  {
+    "revision": "15df28c413151b6da8b7",
+    "url": "/shouldibuythis/static/js/runtime-main.3d00ae93.js"
+  }
+]);
---
+++ static/js/main.f2ed4997.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpshouldibuythis=this.webpackJsonpshouldibuythis||[]).push([[0],[function(s,i,h){s.exports=h(1)},function(s,i){}],[[0,1]]]);
+//# sourceMappingURL=main.f2ed4997.chunk.js.map
